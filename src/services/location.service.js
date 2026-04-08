447a68669ac603448576a6c2332751a07c82c911
Gurusharan Singh <gurusharansingh2222@gmail.com>
2026-04-08 19:33:15 +0530

    new update 1.4

--- src/services/location.service.js
+++ src/services/location.service.js
@@ -283,7 +283,7 @@ export async function getLocationsByOwner(ownerId, { page = 1, limit = 20 }) {
               'price_per_hour', pst.price_per_hour,
               'price_per_day', pst.price_per_day,
               'price_per_month', pst.price_per_month,
-              'status',pst.status
+              
             )
           )
           FROM parking_slot_types pst
